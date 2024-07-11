6395592c564cf7a209e1474ee05220c34e499f88
LongMingWei <longmw2001@gmail.com>
2024-07-11 17:07:31 +0800

    A

--- gradiomain.py
+++ gradiomain.py
@@ -1,6 +1,3 @@
-import os
-import datetime
-import pickle
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import cv2
@@ -9,7 +6,6 @@ import face_recognition
 from PIL import Image
 import gradio as gr
 from deepface import DeepFace
-import util
 import sys
 import time
 
@@ -17,11 +13,24 @@ sys.path.append("Silent-Face-Anti-Spoofing")
 from test1 import test  # from test1.py in the Silent-Face-Anti-Spoofing folder
 # gr.themes.builder()
 
-
 uri = "mongodb+srv://longmw2001:g2XoE2TTYOKjGVFg@facerecognition.hwcxpnq.mongodb.net/?retryWrites=true&w=majority&appName=FaceRecognition"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['users']['faces']
+collection = client['users']['faces']
+collection.create_index("username", unique=True)
 
+# Load all embeddings into memory
+user_embeddings = {}
+
+def load_embeddings():
+    global user_embeddings
+    user_embeddings = {}
+    for user in collection.find():
+        username = user['username']
+        embedding = np.array(user['embedding'])
+        user_embeddings[username] = embedding
+        print(username)
+
+load_embeddings()
 
 def recognize_user(image):
     img = np.array(image)
@@ -38,20 +47,23 @@ def recognize_user(image):
         if len(face_recognition.face_encodings(img_rgb)) == 0:
             return "No person detected"
         else:
-            name = recognize(img_rgb, db)
+            name = recognize(img_rgb)
             if name in ['unknown_person', 'no_persons_found']:
                 return "Unknown user. Please register if you have not or try again."
             else:
                 result = DeepFace.analyze(img_rgb, actions=['emotion'], enforce_detection=False)
                 mood = result[0]['dominant_emotion']
+                if name.endswith('_0001') or name.endswith('_0002') or name.endswith('_0003'):
+                    name = name[:-5]
                 return f"Access granted. Welcome, {name}, you look {mood} today!"
     else:
-        name = recognize(img_rgb, db)
+        name = recognize(img_rgb)
         if name in ['unknown_person', 'no_persons_found']:
             return "Hello unknown fake NPC. You shall not pass!"
         else:
+            if name.endswith('_0001') or name.endswith('_0002') or name.endswith('_0003'):
+                name = name[:-5]
             return f"You think you are {name}? You shall not pass!"
-
 
 def register_user(name, image):
     if not name:
@@ -61,27 +73,27 @@ def register_user(name, image):
     embeddings = face_recognition.face_encodings(img)
     if len(embeddings) == 0:
         return "No face detected in the image."
-    embeddings = embeddings[0].tolist()  # Convert to list for MongoDB storage
+    embedding = embeddings[0].tolist()  # Convert to list for MongoDB storage
     document = {
         'username': name,
-        'embedding': embeddings
+        'embedding': embedding
     }
-    db.insert_one(document)
+    collection.insert_one(document)
+    user_embeddings[name] = embeddings[0]  # Add new data entry to memory
     return f'User {name} registered successfully!'
-
 
 def delete_user(name):
     if not name:
         return "Enter user name to delete."
 
-    result = db.delete_one({'username': name})
+    result = collection.delete_one({'username': name})
     if result.deleted_count > 0:
+        user_embeddings.pop(name)  # Remove data entry from memory
         return f'User {name} deleted successfully!'
     else:
         return f'User {name} not found!'
 
-
-def recognize(img, db):
+def recognize(img):
     embeddings_unknown = face_recognition.face_encodings(img)
     if len(embeddings_unknown) == 0:
         return 'no_persons_found'
@@ -91,18 +103,14 @@ def recognize(img, db):
     match = False
     threshold = 0.6
     mini = threshold
-    for user in db.find():
-        embeddings = np.array(user['embedding'])
-        distance = face_recognition.face_distance([embeddings], embeddings_unknown)[0]
-        if distance < threshold and distance < mini:
+    min_dis_id = 'unknown_person'
+    for username, embedding in user_embeddings.items():
+        distance = face_recognition.face_distance([embedding], embeddings_unknown)[0]
+        if distance < mini:
             mini = distance
-            min_dis_id = user['username']
+            min_dis_id = username
             match = True
-    if match:
-        return min_dis_id
-    else:
-        return 'unknown_person'
-
+    return min_dis_id if match else 'unknown_person'
 
 # Frontend interface
 theme = gr.themes.Soft(
